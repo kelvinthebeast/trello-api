9b7c0e4d65a8c570fa6a5433a6345f2d702a4d52
Thành Nhân <117796054+kelvinthebeast@users.noreply.github.com>
2025-03-05 16:53:52 +0700

    init base project

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,1 +1,7 @@
 ///
+// elliotlee
+// Eka5bX5kudQ695wh
+
+const MONGO_URL = 'mongodb+srv://elliotlee:Eka5bX5kudQ695wh@cluster0.hjmq4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+const DATABASE_NAME ='trello-web-api'
